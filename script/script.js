6a3e271a4ced2795d4537e82016a210a7f36c354
Matt Zhao <46049501+JediRebel@users.noreply.github.com>
2023-02-01 03:14:28 -0400

    more codes

--- script/script.js
+++ script/script.js
@@ -10,8 +10,43 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// Generate related strings with functions
+
+const numberGenerator = function (){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+
+const lowerCaseGenerator = function (){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const UpperCaseGenerator = function (){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const specialCharGenerator = function (){ 
+  const specialChar = '!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~'; 
+  return specialChar[Math.floor(Math.random() * specialChar.length)];
+};
+
+console.log(specialCharGenerator());
+
+
+// Select the options page and add open/close actions;
+const options = document.querySelector('.options');
+
+const openOptions = document.querySelector('#openOptions');
+const closeOptions = document.querySelector('#closeOptions');
+
+// A click on the bottom botton opens the options page;
+openOptions.addEventListener('click', function() {
+  options.showModal();
+});
+// After selecting the options, click the button to generate a password;
+closeOptions.addEventListener('click', function(){
+  options.close();
+});
 
 
 
