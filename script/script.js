8c8e155acae3700c6e2809dab27be0fad381fe09
Matt Zhao <46049501+JediRebel@users.noreply.github.com>
2023-02-02 18:09:46 -0400

    fixed some errors

--- script/script.js
+++ script/script.js
@@ -18,16 +18,16 @@ closeOptions.addEventListener('click', function(){
 
 
 // Length data of the password
-const charNumberELT = document.getElementById('charNumber');
+const charLength = document.getElementById('charLength');
 
 // get elments for options to add to the password
-const numbersELT = document.getElementById('numbers');
 const lowerCaseELT = document.getElementById('lowerCase');
 const upperCaseELT = document.getElementById('upperCase');
+const numbersELT = document.getElementById('numbers');
 const specialsELT = document.getElementById('specials');
 
 // get the password text area 
-const passwordArea = document.getElementById('password');
+const pswArea = document.getElementById('pswArea');
 
 // get the passport generator buttom
 
@@ -35,55 +35,65 @@ const passwordBtn = document.getElementById('closeOptions');
 
 
 // Generate character array
+// var numberCodes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; 
+// var lowerCaserCodes = ['a', 'b', 'c', 'd', 'e','','','',]
 var numberCodes = []; 
 var lowerCaserCodes = [];
 var upperCaserCodes = [];
 var specialerCodes = [];
 
-numberCodes = ArrayNumtoChar(48, 57);
 lowerCaserCodes = ArrayNumtoChar(65, 90);
 upperCaserCodes = ArrayNumtoChar(97, 122);
+numberCodes = ArrayNumtoChar(48, 57);
 specialerCodes = ArrayNumtoChar(33, 47).concat(ArrayNumtoChar(58, 64)).concat(ArrayNumtoChar(91, 96)).concat(ArrayNumtoChar(123, 126));
 
 
 // Array function to get characters. The ASCII Character Set: https://www.w3schools.com/charsets/ref_html_ascii.asp 
 
-function ArrayNumtoChar (start, end) {
+function ArrayNumtoChar (from, to) {
   let array = [];
-  for (let i = start; i <= end; i++){
+  for (let i = from; i <= to; i++){
     array.push(i)
+  }
   return array
   }
-}
 
-function generatePassword(numberChecked, lowerChecked, uperChecked, uperChecked, specialChecked) {
+function generatePassword(length,lowerChecked, uperChecked, numberChecked, specialChecked) {
   let passwordCode = [];
-  if (numberChecked) passwordCode = passwordCode.concat(numberCodes);
   if (lowerChecked) passwordCode = passwordCode.concat(lowerCaserCodes);
   if (uperChecked) passwordCode = passwordCode.concat(upperCaserCodes);
+  if (numberChecked) passwordCode = passwordCode.concat(numberCodes);
   if (specialChecked) passwordCode = passwordCode.concat(specialerCodes); 
 
-const passwordChar = []
+  const passwordChar = [];
 for (let i = 8; i < length; i++){
   const RandomCharCode = passwordCode[Math.floor(Math.random() * passwordCode.length)];
-
+  
   passwordChar.push(String.fromCharCode(RandomCharCode))
-
 }
+    return passwordChar.join('');
 }
 
-passwordBtn.addEventListener('click', function () {
-  const length = +charNumberELT.value;
-  const numberChecked = numbersELT.checked;
+
+pswArea.addEventListener('submit', function() {
+  
+  const length = charLength.value;
   const lowerChecked = lowerCaseELT.checked;
   const uperChecked = upperCaseELT.checked;
+  const numberChecked = numbersELT.checked;
   const specialChecked = specialsELT.checked;
+
   if (numberChecked === false && lowerChecked === false && uperChecked === false && specialChecked === false) {
     alert ("Please Select at least One Option!")
-  }  else{
-  const password = generatePassword(length, numberChecked, lowerChecked, uperChecked,specialChecked);
-  passwordArea.innerText = password;}}
-  )
+  } 
+  else
+   {
+  const password = generatePassword(length, lowerChecked, uperChecked, numberChecked, specialChecked);
+  passwordArea.innerText = password;
+}
+})
+
+
 
   // This also works as a reminder to select at least one option, but kind of annoying.
 // document.getElementById("closeOptions").onmouseover = function() {getAlert()};
